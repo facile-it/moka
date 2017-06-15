3cb84422dd452b800c2d9da69f27c1bdce70064b
Angelo Giuffredi <agiuffredi@gmail.com>
2017-06-15 09:49:16 +0200

    Enforce type hinting to StubSet::add(Stub )

--- src/Moka/Stub/StubSet.php
+++ src/Moka/Stub/StubSet.php
@@ -3,8 +3,34 @@ declare(strict_types=1);
 
 namespace Moka\Stub;
 
+use Moka\Exception\InvalidArgumentException;
 use PhpCollection\Set;
 
+/**
+ * Class StubSet
+ * @package Moka\Stub
+ */
 final class StubSet extends Set
 {
+    /**
+     * @param object|\PhpCollection\scalar $elem
+     * @return void
+     *
+     * @throws InvalidArgumentException
+     */
+    public function add($elem)
+    {
+        if (!$elem instanceof Stub) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'The first parameter must be an instance of %s but of type %s given',
+                    Stub::class,
+                    gettype($elem)
+                )
+            );
+        }
+
+        parent::add($elem);
+    }
+
 }

85251d1eef36cb2214282418e908d7329c2d9391
Alberto Villa <villa.alberto@gmail.com>
2017-06-23 18:47:04 +0200

    Simplify tests

--- tests/Strategy/FakeMockingStrategy.php
+++ tests/Strategy/FakeMockingStrategy.php
@@ -3,28 +3,10 @@ declare(strict_types=1);
 
 namespace Tests\Strategy;
 
-use Moka\Strategy\AbstractMockingStrategy;
-use Moka\Stub\Stub;
-
-class FakeMockingStrategy extends AbstractMockingStrategy
+class FakeMockingStrategy extends IncompleteMockingStrategy
 {
     public function __construct()
     {
         self::checkDependencies('FooBar' . mt_rand(), 'php:^1.0');
-    }
-
-    protected function doBuild(string $fqcn)
-    {
-        // TODO: Implement doBuild() method.
-    }
-
-    protected function doDecorate($mock, Stub $stub)
-    {
-        // TODO: Implement doDecorate() method.
-    }
-
-    protected function doGet($mock)
-    {
-        // TODO: Implement doGet() method.
     }
 }

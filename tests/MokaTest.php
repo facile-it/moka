04256d8b0a9e92d660319454f5eb6cf18d231c17
Alberto Villa <villa.alberto@gmail.com>
2017-06-23 18:29:46 +0200

    Update test

--- tests/MokaTest.php
+++ tests/MokaTest.php
@@ -5,41 +5,27 @@ namespace Tests;
 
 use Moka\Exception\NotImplementedException;
 use Moka\Moka;
-use Moka\Plugin\Mockery\MockeryMockingStrategy;
-use Moka\Plugin\PHPUnit\PHPUnitMockingStrategy;
-use Moka\Plugin\Prophecy\ProphecyMockingStrategy;
 use Moka\Proxy\Proxy;
-use Moka\Strategy\MockingStrategyInterface;
 use PHPUnit\Framework\TestCase;
 
 class MokaTest extends TestCase
 {
-    const BUILDERS = ['mockery', 'phake', 'prophecy', 'phpunit'];
+    /**
+     * Moka::brew() is deprecated, but it still needs to be tested.
+     */
+    const METHODS = ['brew', 'mockery', 'phake', 'phpunit', 'prophecy'];
 
     public function testBrewSuccess()
     {
-        $builders = [
-            new MockeryMockingStrategy(),
-            new PHPUnitMockingStrategy(),
-            new ProphecyMockingStrategy()
-        ];
-
-        foreach ($builders as $builder) {
-            $this->brewWithBuilder($builder);
-        }
-    }
-
-    public function testBrewWithBuilderSuccess()
-    {
-        foreach (self::BUILDERS as $builder) {
+        foreach (self::METHODS as $method) {
             $this->assertInstanceOf(
                 Proxy::class,
-                Moka::$builder(\stdClass::class)
+                Moka::$method(\stdClass::class)
             );
         }
     }
 
-    public function testBrewWithFakeBuilderFailure()
+    public function testBrewFailure()
     {
         $this->expectException(NotImplementedException::class);
 
@@ -53,13 +39,5 @@ class MokaTest extends TestCase
         $proxy2 = Moka::phpunit(\stdClass::class);
 
         $this->assertNotSame($proxy1, $proxy2);
-    }
-
-    protected function brewWithBuilder(MockingStrategyInterface $builder)
-    {
-        $this->assertInstanceOf(
-            Proxy::class,
-            Moka::phpunit(\stdClass::class, null, $builder)
-        );
     }
 }

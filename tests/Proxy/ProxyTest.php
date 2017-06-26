e9525d58c14223c633efe29409dc58c9f82f8ea8
Alberto Villa <villa.alberto@gmail.com>
2017-06-26 12:40:25 +0200

    Really test method forwarding

--- tests/Proxy/ProxyTest.php
+++ tests/Proxy/ProxyTest.php
@@ -9,6 +9,7 @@ use Moka\Proxy\Proxy;
 use Moka\Strategy\MockingStrategyInterface;
 use PHPUnit\Framework\TestCase;
 use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use Tests\BarTestClass;
 use Tests\FooTestClass;
 
 class ProxyTest extends TestCase
@@ -25,8 +26,11 @@ class ProxyTest extends TestCase
 
     protected function setUp()
     {
-        $mock = $this->getMockBuilder(\stdClass::class)
+        $mock = $this->getMockBuilder(BarTestClass::class)
             ->getMock();
+
+        $mock->method('getInt')
+            ->willReturn(1138);
 
         $this->mockingStrategy = $this->getMockBuilder(MockingStrategyInterface::class)
             ->getMock();
@@ -39,6 +43,19 @@ class ProxyTest extends TestCase
             FooTestClass::class,
             $this->mockingStrategy
         );
+    }
+
+    public function testMethodForwardSuccess()
+    {
+        $this->assertSame(1138, $this->proxy->getInt());
+    }
+
+    public function testMethodForwardFailure()
+    {
+        $this->expectException(\Error::class);
+        $this->expectExceptionMessageRegExp('/^Call to undefined method/');
+
+        $this->proxy->fakeMethod();
     }
 
     public function testStubSuccess()

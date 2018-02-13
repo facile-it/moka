81b3f5db138e0f85b0080f058147aead9c9b0faa
Alberto Villa <villa.alberto@gmail.com>
2018-02-13 12:04:57 +0100

    Remove comments from test class

--- tests/Plugin/Prophecy/Token/UnsaltedPriorityToken.php
+++ tests/Plugin/Prophecy/Token/UnsaltedPriorityToken.php
@@ -5,23 +5,13 @@ namespace Tests\Plugin\Prophecy\Token;
 
 use Moka\Plugin\Prophecy\Token\AbstractPriorityToken;
 
-/**
- * Class UnsaltedPriorityToken
- * @package Tests\Plugin\Prophecy\Token
- */
 class UnsaltedPriorityToken extends AbstractPriorityToken
 {
-    /**
-     * UnsaltedPriorityToken constructor.
-     */
     public function __construct()
     {
         // No call to parent::__construct().
     }
 
-    /**
-     * @return int
-     */
     protected function getPriority(): int
     {
         return 0;

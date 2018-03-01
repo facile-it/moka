28f16796011f973cc07d665fbbf627b11224fb35
Angelo Giuffredi <agiuffredi@gmail.com>
2018-03-01 22:47:44 +0100

    Split hof for generating plugin FQCN

--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/Plugin/loadPlugin.php';
+require_once __DIR__ . '/Plugin/generatePluginFQCN.php';
 require_once __DIR__ . '/Factory/buildProxy.php';
 require_once __DIR__ . '/Factory/createStubs.php';
 require_once __DIR__ . '/Factory/createStub.php';
